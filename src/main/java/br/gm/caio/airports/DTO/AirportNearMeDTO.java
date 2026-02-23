1c9854aeb006f8e631ebd9425d0065ca30ea310a
digma <digma@172.16.1.41>
2026-02-23 17:02:36 -0300

    Terminando o pdf 5

---
+++ src/main/java/br/gm/caio/airports/DTO/AirportNearMeDTO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gm.caio.airports.DTO;
+
+/**
+ *
+ * @author digma
+ */
+public class AirportNearMeDTO {
+    
+    private long id;
+    private String name;
+    private String city;
+    private String iataCode;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private double distanciaKM ;
+    
+}
