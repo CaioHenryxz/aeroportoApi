7c34cba8fc8d1bf8cb6dc494cb2de754538b72de
digma <digma@172.16.1.41>
2026-02-23 09:55:36 -0300

    Término da Fase 2 –Implementado endpoint Airport -Add interface AirportRepository -Add classe AirportService -Add classe AirportController

---
+++ src/main/java/br/gm/caio/airports/controllers/AirportController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gm.caio.airports.controllers;
+
+import br.gm.caio.airports.entities.Airport;
+import br.gm.caio.airports.service.AirportService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author digma
+ */
+@RestController
+public class AirportController {
+    
+    @Autowired
+    private AirportService airportService;
+    
+    @GetMapping("/airport")
+    public List<Airport> findAll(){
+        List<Airport> result = airportService.findAll();
+        return result;
+    }
+    
+}
